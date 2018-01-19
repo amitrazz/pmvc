7d0fe0d1b07743beb8cc2138336d6ea866364c90
Amit kumar <amitmishra4893@gmail.com>
2018-01-19 16:19:31 +0530

    Add files via upload

--- app/views/index.php
+++ app/views/index.php
@@ -1,1 +1,35 @@
-Home page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+  <title>PMVC</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="<?php URLROOt; ?>">PMVC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php URLROOt; ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+  </div>
+</nav>
+<div class="container">
+<div class="wrap">
+
+<div class="jumbotron">
+  <h1 class="display-3">Hello, There!</h1>
+  <p class="lead">This is a simple PHP MVC frame which You can use to develope large scale Php application  .</p>
+  <hr class="my-4">
+</div>
+</div>
+</div>
+</body>
+</html>
