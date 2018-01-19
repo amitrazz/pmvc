ac1e50c96d20bba6fdff6d2fc45baf9cd9a2b158
Amit kumar <amitmishra4893@gmail.com>
2018-01-19 15:49:46 +0530

    Add files via upload

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,9 @@
 <?php
+//Db prams
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','ciapanel');
 //app root
 define('APPROOT',dirname(dirname(__FILE__)));
 define('URLROOt','http://localhost/pmvc');
